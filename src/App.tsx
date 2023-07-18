b0252d023518133f0a7747f268307838aa8d8223
Martin Barros <martinbarros86@gmail.com>
2023-07-18 01:26:44 -0300

    Add small API test service

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { testService } from './services';
 import { Home, NotFound } from './pages';
 
 function App() {
+  useEffect(() => {
+    testService.testApi().then((data) => console.log('data: ', data));
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
