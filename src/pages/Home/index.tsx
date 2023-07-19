02aa5400293aff4975a1e38d080565ca691cb5a0
Martin Barros <martinbarros86@gmail.com>
2023-07-19 11:40:28 -0300

    Add MainLayout to all pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,7 @@ import whiteTopRightArrow from '../../assets/images/ArrowTopRight.svg';
 import orangeTopRightArrow from '../../assets/images/OrangeArrowTopRight.svg';
 import graphImageFlying from '../../assets/images/graph-image-flying.png';
 import graphImageStanding from '../../assets/images/graph-image-standing.png';
+import { MainLayout } from '../../components/layouts';
 
 function Home() {
   const handleButtonLogInToVCAccount = () => {
@@ -21,51 +22,53 @@ function Home() {
   };
 
   return (
-    <div className={styles.blockContainer}>
-      <img
-        className={styles.imgTopRight}
-        src={graphImageFlying}
-        alt="graphImageStanding"
-      />
-      <img
-        className={styles.imgBotLeft}
-        src={graphImageStanding}
-        alt="graphImageStanding"
-      />
-      <div className={styles.mainContent}>
-        <img className={styles.logotype} src={Logotype} alt="Logotype" />
-        <h1 className={styles.h1}>
-          Which <span className={styles.textOrange}>account</span> do you want
-          to log in to?
-        </h1>
-        <div className={styles.buttonWrap}>
-          <Button
-            text="Log In To VC Account"
-            icon={<img src={whiteTopRightArrow} alt="Arrow" />}
-            backgroundColor="#F1511B"
-            textColor="#FFF"
-            onClick={handleButtonLogInToVCAccount}
-          />
-          <Button
-            text="Log In To Founder Account"
-            icon={<img src={orangeTopRightArrow} alt="Arrow" />}
-            borderColor="#F1511B"
-            textColor="#F1511B"
-            onClick={handleButtonLogInToFounderAccount}
-          />
-        </div>
-        <Input
-          style="toggle"
-          label="True or False"
-          id="true-or-false"
-          onChange={handleInputChange}
+    <MainLayout>
+      <div className={styles.blockContainer}>
+        <img
+          className={styles.imgTopRight}
+          src={graphImageFlying}
+          alt="graphImageStanding"
         />
-        {/* <div>
-          <Input type="default" placeholder="Enter text" label="Password" id="passwod" onChange={handleInputChange} />
-          <Input type="upload" label="Upload File" id="upload" onChange={handleInputChange} />
-        </div> */}
+        <img
+          className={styles.imgBotLeft}
+          src={graphImageStanding}
+          alt="graphImageStanding"
+        />
+        <div className={styles.mainContent}>
+          <img className={styles.logotype} src={Logotype} alt="Logotype" />
+          <h1 className={styles.h1}>
+            Which <span className={styles.textOrange}>account</span> do you want
+            to log in to?
+          </h1>
+          <div className={styles.buttonWrap}>
+            <Button
+              text="Log In To VC Account"
+              icon={<img src={whiteTopRightArrow} alt="Arrow" />}
+              backgroundColor="#F1511B"
+              textColor="#FFF"
+              onClick={handleButtonLogInToVCAccount}
+            />
+            <Button
+              text="Log In To Founder Account"
+              icon={<img src={orangeTopRightArrow} alt="Arrow" />}
+              borderColor="#F1511B"
+              textColor="#F1511B"
+              onClick={handleButtonLogInToFounderAccount}
+            />
+          </div>
+          <Input
+            style="toggle"
+            label="True or False"
+            id="true-or-false"
+            onChange={handleInputChange}
+          />
+          {/* <div>
+            <Input type="default" placeholder="Enter text" label="Password" id="passwod" onChange={handleInputChange} />
+            <Input type="upload" label="Upload File" id="upload" onChange={handleInputChange} />
+          </div> */}
+        </div>
       </div>
-    </div>
+    </MainLayout>
   );
 }
 
