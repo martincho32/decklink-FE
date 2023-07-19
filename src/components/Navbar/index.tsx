b1607584c1d79fedba3bcbea07e32e231925c7e9
Martin Barros <martinbarros86@gmail.com>
2023-07-19 15:11:29 -0300

    pass useIsleggoed as prop

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Hamburguer, Logo, Wordmark } from '../icons';
 
-function Navbar() {
+interface Props {
+  isUserLogged: boolean;
+}
+
+function Navbar({ isUserLogged = false }: Props) {
   const onClickHamburguer = () => {
     console.log('clicked hamburguer menu!');
   };
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [isUserLogged, setIsUserLogged] = useState(false);
   return (
     <nav className="flex justify-between border border-persimmon rounded p-2 md:py-2 md:px-5">
       <div className="self-center flex gap-0.7">
