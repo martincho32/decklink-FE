a8c4f1892ecf1aa4755057d3e7ff7e900ed00fb3
Martin Barros <martinbarros86@gmail.com>
2023-07-20 10:31:05 -0300

    Add function to buttons

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,26 @@
-import { Link } from 'react-router-dom';
-import { Hamburguer, Logo, Wordmark } from '../icons';
+import { Link, useNavigate } from 'react-router-dom';
+import { Logo, Wordmark } from '../icons';
+import HamburguerButton from './HamburguerButton';
+import Button from '../../UI/Button';
 
 interface Props {
   isUserLogged: boolean;
 }
 
 function Navbar({ isUserLogged = false }: Props) {
+  const navigate = useNavigate();
   const onClickHamburguer = () => {
     console.log('clicked hamburguer menu!');
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleButtonLogIn = (): void => {
+    navigate('/login');
+  };
+  const handleButtonSignUp = (): void => {
+    navigate('/signup');
+  };
+  const handleButtonLogout = (): void => {};
+
   return (
     <nav className="flex justify-between border border-persimmon rounded p-2 md:py-2 md:px-5">
       <div className="self-center flex gap-0.7">
@@ -18,21 +28,39 @@ function Navbar({ isUserLogged = false }: Props) {
         <Wordmark />
       </div>
       <div className={isUserLogged ? 'self-center hidden md:block' : 'hidden'}>
-        <span className="text-persimmon mr-2 inline-block">MyDecks</span>
-        <span>Other Link</span>
+        <Link to="/" className="text-persimmon mr-2 inline-block">
+          MyDecks
+        </Link>
+        <Link to="/">Other Link</Link>
       </div>
-      <div className="hidden md:block">
-        <button className="bg-persimmon text-white p-2" type="button">
-          <Link to="/login">{isUserLogged ? 'LOGOUT' : 'LOGIN'}</Link>
-        </button>
+      <div className="hidden md:flex gap-1 p-1">
+        {!isUserLogged ? (
+          <>
+            <Button
+              text="Log In"
+              icon={<Logo color="#FFFFFF" width="10" height="11" />}
+              backgroundColor="#F1511B"
+              textColor="#FFF"
+              onClick={handleButtonLogIn}
+            />
+            <Button
+              text="Sign In"
+              icon={<Logo color="#FFFFFF" width="10" height="11" />}
+              backgroundColor="#F1511B"
+              textColor="#FFF"
+              onClick={handleButtonLogout}
+            />
+          </>
+        ) : (
+          <Button
+            text="Log Out"
+            icon={<Logo />}
+            textColor="#F1511B"
+            onClick={handleButtonSignUp}
+          />
+        )}
       </div>
-      <button
-        type="button"
-        onClick={onClickHamburguer}
-        className="block md:hidden"
-      >
-        <Hamburguer />
-      </button>
+      <HamburguerButton onClickHamburguer={onClickHamburguer} />
     </nav>
   );
 }
