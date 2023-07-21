7dd6afc6371ebd162a964ffe22448cd44ca571d6
Maxim Chuprynsky <4oper321@gmail.com>
2023-07-21 16:53:50 +0300

    fix login route

--- src/services/login.ts
+++ src/services/login.ts
@@ -5,5 +5,4 @@ const resource = 'auth';
 export const registerUser = (user, role = 'USER') =>
   api.post(`${resource}/register?role=${role}`, user);
 
-export const loginUser = (user, role = 'USER') =>
-  api.post(`${resource}/register?role=${role}`, user);
+export const loginUser = (user) => api.post(`${resource}/login`, user);
