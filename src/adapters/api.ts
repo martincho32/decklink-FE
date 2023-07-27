10338e41b40994b4d7738a99c572683461daa46f
Martin Barros <martinbarros86@gmail.com>
2023-07-27 12:22:23 -0300

    Add config option parameter to post request

--- src/adapters/api.ts
+++ src/adapters/api.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const axiosApi = axios.create({
   baseURL: 'http://localhost:3000/api',
@@ -10,7 +10,11 @@ const basePath = 'http://localhost:3000/api';
 
 const api = {
   get: (endpoint: string) => axiosApi.get(`/${endpoint}`),
-  post: (endpoint: string, body) => axiosApi.post(`/${endpoint}`, body),
+  post: (
+    endpoint: string,
+    body,
+    config: AxiosRequestConfig<any> | undefined = undefined
+  ) => axiosApi.post(`/${endpoint}`, body, config),
   put: (endpoint: string, body) =>
     axiosApi.put(`${basePath}/${endpoint}`, body),
   delete: (endpoint: string) => axiosApi.delete(`${basePath}/${endpoint}`),
--- src/services/deck.ts
+++ src/services/deck.ts
@@ -1,10 +1,16 @@
+import { AxiosRequestConfig } from 'axios';
 import { api } from '../adapters/api';
 
 const resource = 'deck';
 
 export const getDecksByUserId = () => api.get(resource).then((data) => data);
 
-export const createDeck = (deck) => api.post(resource, deck);
+export const createDeck = (
+  deck,
+  config: AxiosRequestConfig<any> | undefined = undefined
+) => {
+  return api.post(resource, deck, config);
+};
 
 export const getDeckByCustomLink = (customLink: string) =>
   api.get(`${resource}/custom/${customLink}`);
