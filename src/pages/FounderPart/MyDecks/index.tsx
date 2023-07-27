5a3b62341f4e5bbf1c031c8733b2894c3e346e00
colorfulpony <4oper321@gmail.com>
2023-07-27 23:28:06 +0300

    Add Fall Back component for empty response from BE  of any Item

--- src/pages/FounderPart/MyDecks/index.tsx
+++ src/pages/FounderPart/MyDecks/index.tsx
@@ -1,31 +1,51 @@
+import { useState } from 'react';
 import { MainLayout, Button } from '../../../components';
 import whiteTopRightArrow from '../../../assets/images/ArrowTopRight.svg';
 import styles from './MyDecks.module.css';
 import Card from '../../../components/FounderPart/MyDecks/Card';
+import EmptyState from '../../../components/ItemEmptyState';
 
 function MyDecks() {
+  const [dataIsLoaded, setDataIsLoaded] = useState<boolean>(false);
+
   return (
     <MainLayout>
-      <div className={styles.myDesksWrapper}>
-        <div className={styles.pageNavigation}>
-          <h2 className={styles.title}>
-            <span>Hi Ben!</span> Here is your created decks
-          </h2>
-          <Button
-            type="button"
-            text="Create New Deck"
-            icon={<img src={whiteTopRightArrow} alt="Arrow" />}
-            backgroundColor="#F1511B"
-            textColor="#FFF"
-          />
+      {dataIsLoaded ? (
+        <div className={styles.myDesksWrapper}>
+          <div className={styles.pageNavigation}>
+            <h2 className={styles.title}>
+              <span>Hi Ben!</span> Here is your created decks
+            </h2>
+            <Button
+              type="button"
+              text="Create New Deck"
+              icon={<img src={whiteTopRightArrow} alt="Arrow" />}
+              backgroundColor="#F1511B"
+              textColor="#FFF"
+            />
+          </div>
+          <div className={styles.decksBlock}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
         </div>
-        <div className={styles.decksBlock}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-      </div>
+      ) : (
+        <EmptyState
+          title="Zero pitchdecks at the moment."
+          subtitle="Why not be the first to create one and kickstart the fun?"
+          button={
+            <Button
+              type="button"
+              text="Create New Deck"
+              icon={<img src={whiteTopRightArrow} alt="Arrow" />}
+              backgroundColor="#F1511B"
+              textColor="#FFF"
+            />
+          }
+        />
+      )}
     </MainLayout>
   );
 }
