f12604d7e38ba37dbc2907b22dbf1fd6b374291a
Martin Barros <martinbarros86@gmail.com>
2023-07-27 22:53:04 -0300

    Add headres, patch method and getById method

--- src/adapters/api.ts
+++ src/adapters/api.ts
@@ -4,6 +4,13 @@ import axios, { AxiosRequestConfig } from 'axios';
 const axiosApi = axios.create({
   baseURL: 'http://localhost:3000/api',
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
+    'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS',
+  },
 });
 
 const basePath = 'http://localhost:3000/api';
@@ -15,8 +22,16 @@ const api = {
     body,
     config: AxiosRequestConfig<any> | undefined = undefined
   ) => axiosApi.post(`/${endpoint}`, body, config),
-  put: (endpoint: string, body) =>
-    axiosApi.put(`${basePath}/${endpoint}`, body),
+  put: (
+    endpoint: string,
+    body,
+    config: AxiosRequestConfig<any> | undefined = undefined
+  ) => axiosApi.put(`${basePath}/${endpoint}`, body, config),
+  patch: (
+    endpoint: string,
+    body,
+    config: AxiosRequestConfig<any> | undefined = undefined
+  ) => axiosApi.patch(`${basePath}/${endpoint}`, body, config),
   delete: (endpoint: string) => axiosApi.delete(`${basePath}/${endpoint}`),
 };
 
--- src/services/deck.ts
+++ src/services/deck.ts
@@ -5,6 +5,20 @@ const resource = 'deck';
 
 export const getDecksByUserId = () => api.get(resource).then((data) => data);
 
+export const getDeckById = (
+  id: string
+): Promise<{
+  data: {
+    customDeckLink: string;
+    deckUrl: string;
+    name: string;
+    password: string;
+    requestEmail: boolean;
+    requestPassword: boolean;
+    slides: number;
+  };
+}> => api.get(`${resource}/${id}`).then((data) => data);
+
 export const createDeck = (
   deck,
   config: AxiosRequestConfig<any> | undefined = undefined
@@ -12,7 +26,15 @@ export const createDeck = (
   return api.post(resource, deck, config);
 };
 
+export const editDeck = (
+  deck,
+  id,
+  config: AxiosRequestConfig<any> | undefined = undefined
+) => {
+  return api.patch(`${resource}/${id}`, deck, config);
+};
+
 export const getDeckByCustomLink = (customLink: string) =>
   api.get(`${resource}/custom/${customLink}`);
 
-// export const deleteItem = (itemId) => api.delete(`${resource}/${itemId}`);
+export const deleteDeck = (deckId) => api.delete(`${resource}/${deckId}`);
