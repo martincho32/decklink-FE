0a6b59294af5e651c55abcaa3990021492314c1c
Martin Barros <martinbarros86@gmail.com>
2023-07-30 23:30:11 -0300

    Add DeckDetail page

--- src/pages/FounderPart/DeckDetail/index.tsx
+++ src/pages/FounderPart/DeckDetail/index.tsx
@@ -1,9 +1,47 @@
-import { LineChart, MainLayout } from '../../../components';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { MainLayout } from '../../../components';
+import { deckService, deckViewService } from '../../../services';
+import { IDeck, IDeckView } from '../../../types/index';
+import DeckAverageStats from '../../../components/FounderPart/DeckAverageStats';
 
 function DeclkDetail() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [deck, setDeck] = useState<IDeck | null>(null);
+  const [deckViews, setDeckViews] = useState<IDeckView[] | null>(null);
+
+  useEffect(() => {
+    if (!id) {
+      // Handle this, maybe use Snackbar to show error
+      console.error('Error: There is no deck id to get.');
+      navigate('/founder/decks');
+    } else {
+      deckService
+        .getDeckById(id)
+        .then(async ({ data }) => {
+          // Set deck into state
+          setDeck(data);
+          // Pas deck as prop to LineChart
+          // Call deckViews service to get all deckViews by deck ID
+          const response = await deckViewService.getDeckViewByDeckId(id);
+          setDeckViews(response.data);
+          // Do some logic to create Average stats, example: Average time spent viewing each slide by all people
+          // Pass data as props to LineChart componennt, or mabe create a component wraps LineChart?
+        })
+        .catch((error) => {
+          console.error('Error: ', error.message);
+        });
+    }
+  }, []);
+
   return (
     <MainLayout>
-      <LineChart />
+      {/* One component for global deck viewing statistics */}
+      <DeckAverageStats deckViews={deckViews} deck={deck} />
+      {/* One component for individual deck viewing statistics */}
+      {/* <LineChart deckViews={deckViews} deck={deck} /> */}
     </MainLayout>
   );
 }
