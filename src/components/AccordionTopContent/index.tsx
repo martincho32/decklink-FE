cb9169ff980385804a6d25bb2718db383a01027f
Martin Barros <martinbarros86@gmail.com>
2023-08-02 12:26:30 -0300

    Styles fixes

--- src/components/AccordionTopContent/index.tsx
+++ src/components/AccordionTopContent/index.tsx
@@ -10,34 +10,25 @@ interface Props {
 }
 function AccordionTopContent({ email, date, totalViewingTime }: Props) {
   return (
-    <div className="flex-1">
-      <div className="flex justify-between">
-        <div className="flex flex-col grow">
-          <span
-            style={{ borderBottom: '1px solid' }}
-            className="text-gray-600 border-mirage pb-4"
-          >
+    <div className="flex-1 grow-2.5 lg:pr-5">
+      <div className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between">
+        <div className="flex md:flex-col justify-between grow">
+          <span className="text-gray-600 md:border-solid md:border-b md:border-mirage md:pb-4 font-normal">
             User Email
           </span>
-          <span className="pt-4">{email}</span>
+          <span className="md:pt-4">{email}</span>
         </div>
-        <div className="flex flex-col grow">
-          <span
-            style={{ borderBottom: '1px solid' }}
-            className="text-gray-600 border-mirage pb-4"
-          >
+        <div className="flex md:flex-col justify-between grow">
+          <span className="text-gray-600 md:border-solid md:border-b md:border-mirage md:pb-4 font-normal">
             Date of View
           </span>
-          <span className="pt-4">{timestampToFormattedDate(date)}</span>
+          <span className="md:pt-4">{timestampToFormattedDate(date)}</span>
         </div>
-        <div className="flex flex-col grow">
-          <span
-            style={{ borderBottom: '1px solid' }}
-            className="text-gray-600 border-mirage pb-4"
-          >
+        <div className="flex md:flex-col justify-between grow">
+          <span className="text-gray-600 md:border-solid md:border-b md:border-mirage md:pb-4 font-normal text-end">
             Total Viewing time
           </span>
-          <span className="pt-4">
+          <span className="md:pt-4 text-end">
             {milisecondsToMinutesAndSeconds(totalViewingTime)}
           </span>
         </div>
