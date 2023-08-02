550371c2f84425adf527326b5935b6442d5e83cb
Martin Barros <martinbarros86@gmail.com>
2023-08-02 14:47:54 -0300

    Remove duplicated code

--- src/components/FounderPart/DeckPreview/index.tsx
+++ src/components/FounderPart/DeckPreview/index.tsx
@@ -40,76 +40,103 @@ function DeckPreview({
   };
 
   const onPrev = () => {
-    if (pageNumber === 1) {
-      return;
-    }
     setPageNumber(pageNumber - 1);
   };
 
   const onNext = () => {
-    if (pageNumber === numPages) {
-      return;
-    }
     setPageNumber(pageNumber + 1);
   };
 
   if (!visible) return null;
 
-  if (type === 'deckCreationPreview') {
-    return (
-      <div
-        id="container"
-        role="button"
-        tabIndex={0}
-        onClick={handleOnClose}
-        className="fixedContainer fixed overflow-y-scroll h-screen inset-0 bg-black bg-opacity-80 backdrop-blur-sm p-4"
-      >
-        <Document
-          file={file}
-          onLoadSuccess={onDocumentLoadSuccess}
-          options={options}
-          noData={<h4 className="">No file selected</h4>}
-          className="document h-screen p-4 bg-mirage rounded-lg"
+  const handleOnCloseFormClose = (event) => {
+    if (event.target.id === 'containerClose') {
+      setShowModal(false);
+    }
+  };
+
+  const handleModalSubmit = (email: string, password?: string) => {
+    console.log('Submitted Email:', email);
+    if (password !== '') {
+      console.log('Submitted Password:', password);
+    }
+    setShowModal(false);
+  };
+  return (
+    <div
+      id="container"
+      role="button"
+      tabIndex={0}
+      onClick={handleOnClose}
+      className={`fixedContainer fixed overflow-y-scroll h-screen inset-0 bg-black bg-opacity-80 backdrop-blur-sm ${
+        type === 'deckUserPreview' ? 'p-4' : ''
+      }`}
+    >
+      {type === 'deckUserPreview' && (
+        <button
+          type="button"
+          className="hidden"
+          onClick={() => {
+            setShowModal(true);
+            document.body.style.overflow = 'hidden';
+          }}
         >
-          <div className="flex gap-4 justify-center">
-            {pageNumber === 1 ? (
-              <Button
-                type="button"
-                icon={<Logo color="#f1511b2e" topLeft />}
-                onClick={onPrev}
+          Open Modal
+        </button>
+      )}
+      <AskEmailPassword
+        show={showModal}
+        onClose={handleOnCloseFormClose}
+        onSubmit={handleModalSubmit}
+        // showPasswordInput // You can change this to false if you only want an email input.
+      />
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={options}
+        noData={<h4 className="">No file selected</h4>}
+        className={`document h-screen p-4 bg-mirage ${
+          type === 'deckCreationPreview' ? ' rounded-lg' : 'md:rounded-none'
+        }`}
+      >
+        <div
+          className={`flex gap-4 justify-center ${
+            type === 'deckUserPreview' && 'w-full h-auto'
+          }`}
+        >
+          <Button
+            type="button"
+            icon={
+              <Logo
+                color={pageNumber === 1 ? '#f1511b2e' : '#F1511B'}
+                topLeft
               />
-            ) : (
-              <Button
-                type="button"
-                icon={<Logo color="#F1511B" topLeft />}
-                onClick={onPrev}
-              />
-            )}
+            }
+            onClick={onPrev}
+            disabled={pageNumber === 1}
+          />
 
-            <Page
-              renderTextLayer={false}
-              renderAnnotationLayer={false}
-              className="pageWrapper"
-              pageNumber={pageNumber}
-            />
-
-            {pageNumber === numPages ? (
-              <Button
-                type="button"
-                icon={<Logo color="#f1511b2e" />}
-                className="max-h-min"
-                onClick={onNext}
-              />
-            ) : (
-              <Button
-                type="button"
-                icon={<Logo color="#F1511B" />}
-                className="max-h-min"
-                onClick={onNext}
-              />
-            )}
-          </div>
-          {/* Thumbnails */}
+          <Page
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            className={`${
+              type === 'deckUserPreview'
+                ? 'pageWrapperFullScreen'
+                : 'pageWrapper'
+            }`}
+            pageNumber={pageNumber}
+          />
+          <Button
+            type="button"
+            icon={
+              <Logo color={pageNumber === numPages ? '#f1511b2e' : '#F1511B'} />
+            }
+            className="max-h-min"
+            onClick={onNext}
+            disabled={pageNumber === numPages}
+          />
+        </div>
+        {type === 'deckCreationPreview' && (
           <div className="previewPagesWrapper">
             {Array.from(new Array(numPages), (el, index) => (
               <Thumbnail
@@ -124,105 +151,10 @@ function DeckPreview({
               />
             ))}
           </div>
-        </Document>
+        )}
+      </Document>
 
-        <button
-          className="buttonClose w-8 h-8 fixed top-8 right-8 p-2 bg-persimmon rounded-md"
-          onClick={onClose}
-          type="button"
-        >
-          {/* <img className="buttonCloseIcon w-4 h-4" src={iconTopRight} alt="" /> */}
-          <CloseIcon width="16" height="16" color="#FFFFFF" />
-        </button>
-      </div>
-    );
-  }
-
-  if (type === 'deckUserPreview') {
-    const handleOnCloseFormClose = (event) => {
-      if (event.target.id === 'containerClose') {
-        setShowModal(false);
-      }
-    };
-
-    const handleModalSubmit = (email: string, password?: string) => {
-      console.log('Submitted Email:', email);
-      if (password !== '') {
-        console.log('Submitted Password:', password);
-      }
-      setShowModal(false);
-    };
-    return (
-      <div
-        id="container"
-        role="button"
-        tabIndex={0}
-        onClick={handleOnClose}
-        className="fixedContainer fixed overflow-y-scroll h-screen inset-0 bg-black bg-opacity-80 backdrop-blur-sm"
-      >
-        <button
-          type="button"
-          className="hidden"
-          onClick={() => {
-            setShowModal(true);
-            document.body.style.overflow = 'hidden';
-          }}
-        >
-          Open Modal
-        </button>
-        <AskEmailPassword
-          show={showModal}
-          onClose={handleOnCloseFormClose}
-          onSubmit={handleModalSubmit}
-          // showPasswordInput // You can change this to false if you only want an email input.
-        />
-        <Document
-          file={file}
-          onLoadSuccess={onDocumentLoadSuccess}
-          options={options}
-          noData={<h4 className="">No file selected</h4>}
-          className="document h-screen p-4 bg-mirage md:rounded-none"
-        >
-          <div className="flex w-full h-auto gap-4 justify-center">
-            {pageNumber === 1 ? (
-              <Button
-                type="button"
-                icon={<Logo color="#f1511b2e" topLeft />}
-                onClick={onPrev}
-              />
-            ) : (
-              <Button
-                type="button"
-                icon={<Logo color="#F1511B" topLeft />}
-                onClick={onPrev}
-              />
-            )}
-
-            <Page
-              renderTextLayer={false}
-              renderAnnotationLayer={false}
-              className="pageWrapperFullScreen"
-              pageNumber={pageNumber}
-            />
-
-            {pageNumber === numPages ? (
-              <Button
-                type="button"
-                icon={<Logo color="#f1511b2e" />}
-                className="max-h-min"
-                onClick={onNext}
-              />
-            ) : (
-              <Button
-                type="button"
-                icon={<Logo color="#F1511B" />}
-                className="max-h-min"
-                onClick={onNext}
-              />
-            )}
-          </div>
-        </Document>
-
+      {type === 'deckUserPreview' && (
         <Button
           type="button"
           text="Save This Deck"
@@ -232,18 +164,23 @@ function DeckPreview({
           textColor="#FFF"
           onClick={onSaveDeck}
         />
+      )}
 
-        <button
-          className="buttonClose fixed w-8 h-8 top-8 right-8 p-2 bg-persimmon rounded-md"
-          onClick={onClose}
-          type="button"
-        >
-          {/* <img className="buttonCloseIcon w-4 h-4" src={iconTopRight} alt="" /> */}
-          <CloseIcon width="16" height="16" color="#FFFFFF" />
-        </button>
-      </div>
-    );
-  }
+      {/* <button
+        className="buttonClose fixed w-8 h-8 top-8 right-8 p-2 bg-persimmon rounded-md"
+        onClick={onClose}
+        type="button"
+      >
+        <CloseIcon width="16" height="16" color="#FFFFFF" />
+      </button> */}
+      <Button
+        type="button"
+        icon={<CloseIcon width="16" height="16" color="#FFFFFF" />}
+        className="buttonClose fixed w-8 h-8 top-8 right-8 p-2 bg-persimmon rounded-md"
+        onClick={onClose}
+      />
+    </div>
+  );
 }
 
 export default DeckPreview;
