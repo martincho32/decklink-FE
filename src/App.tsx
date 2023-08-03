4dd5f3c6118885b551013d646c8329b7585a9c8e
Martin Barros <martinbarros86@gmail.com>
2023-08-03 15:12:53 -0300

    Add new route and UIcontextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,34 @@ import {
   DeckCreation,
   DeckEdit,
   DeckDetail,
+  Presentation,
 } from './pages';
 import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './context';
+import { AuthProvider, UIProvider } from './context';
 
 function App() {
   return (
     <AuthProvider>
-      <SnackbarProvider maxSnack={3}>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Landing />} />
-            <Route path="/landing" element={<Landing />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route element={<ProtectedRoute />}>
-              <Route path="/founder/decks" element={<MyDecks />} />
-              <Route path="/founder/deck/create" element={<DeckCreation />} />
-              <Route path="/founder/deck/edit/:id" element={<DeckEdit />} />
-              <Route path="/founder/deck/:id" element={<DeckDetail />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </SnackbarProvider>
+      <UIProvider>
+        <SnackbarProvider maxSnack={3}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Landing />} />
+              <Route path="/landing" element={<Landing />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/founder/decks" element={<MyDecks />} />
+                <Route path="/founder/deck/create" element={<DeckCreation />} />
+                <Route path="/founder/deck/edit/:id" element={<DeckEdit />} />
+                <Route path="/founder/deck/:id" element={<DeckDetail />} />
+                <Route path="/:customDeckLink" element={<Presentation />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </SnackbarProvider>
+      </UIProvider>
     </AuthProvider>
   );
 }
