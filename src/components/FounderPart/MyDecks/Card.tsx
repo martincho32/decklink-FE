8f8ebd420689ef3a1c87ae02cc828fd1ad663990
Martin Barros <martinbarros86@gmail.com>
2023-08-03 15:16:14 -0300

    Update copy to clipboard logic since the used one is about to be deprecated

--- src/components/FounderPart/MyDecks/Card.tsx
+++ src/components/FounderPart/MyDecks/Card.tsx
@@ -39,29 +39,45 @@ function Card({ deck, handleClickDelete }: Props) {
   };
 
   const handleCopyClick = () => {
-    // Create a temporary input element
-    const tempInput = document.createElement('input');
-    tempInput.value = 'decklink.com/'.concat(deck.customDeckLink);
-    document.body.appendChild(tempInput);
+    // // Create a temporary input element
+    // const tempInput = document.createElement('input');
+    // tempInput.value = 'decklink.com/'.concat(deck.customDeckLink);
+    // document.body.appendChild(tempInput);
 
-    // Select the text inside the input element
-    tempInput.select();
-    tempInput.setSelectionRange(0, 99999); // For mobile devices
+    // // Select the text inside the input element
+    // tempInput.select();
+    // tempInput.setSelectionRange(0, 99999); // For mobile devices
 
-    // Copy the text to the clipboard
-    document.execCommand('copy');
+    // // Copy the text to the clipboard
+    // document.execCommand('copy');
 
-    // Remove the temporary input element
-    document.body.removeChild(tempInput);
+    // // Remove the temporary input element
+    // document.body.removeChild(tempInput);
 
-    enqueueSnackbar('You copied link', {
-      variant: 'success',
-      autoHideDuration: 1000,
-      anchorOrigin: {
-        vertical: 'top',
-        horizontal: 'right',
+    // enqueueSnackbar('You copied link', {
+    //   variant: 'success',
+    //   autoHideDuration: 1000,
+    //   anchorOrigin: {
+    //     vertical: 'top',
+    //     horizontal: 'right',
+    //   },
+    // });
+    navigator.clipboard.writeText(`decklink.com/${deck?.customDeckLink}`).then(
+      () => {
+        /* Resolved - text copied to clipboard successfully */
+        enqueueSnackbar('Url successfully copied!!', {
+          variant: 'success',
+          autoHideDuration: 2000,
+          anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'right',
+          },
+        });
       },
-    });
+      (error) => {
+        console.error('Failed to copy: ', error);
+      }
+    );
   };
 
   const handleLoadError = (error: any) => {
