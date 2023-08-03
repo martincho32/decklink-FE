1191c37d02f196a9d1a32b31f4c7cd5e74b54d59
Martin Barros <martinbarros86@gmail.com>
2023-08-03 15:15:36 -0300

    Hide Document if user hasn't passed validations

--- src/components/FounderPart/DeckPreview/index.tsx
+++ src/components/FounderPart/DeckPreview/index.tsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import axios from 'axios';
 import { Page, Document, Thumbnail } from 'react-pdf'; /** File library */
+import { enqueueSnackbar } from 'notistack';
 import './DeckPreview.css';
 import Button from '../../UI/Button';
 import { CloseIcon, Logo } from '../..';
 import AskEmailPassword from '../../AskEmailPassword';
+import { UIContext } from '@/context';
+import { deckService } from '@/services';
 
-function DeckPreview({
-  type,
-  visible,
-  onClose,
-  pageNumber,
-  file,
-  onDocumentLoadSuccess,
-  options,
-  numPages,
-  setPreviewPickDeckSlide,
-  setPageNumber,
-}: {
+export interface Props {
   type: 'deckCreationPreview' | 'deckUserPreview';
-  visible: boolean;
   onClose: () => void;
   pageNumber: number;
   file;
@@ -27,8 +19,24 @@ function DeckPreview({
   numPages;
   setPreviewPickDeckSlide;
   setPageNumber;
-}) {
-  const [showModal, setShowModal] = useState(false);
+  deckId: string | null;
+}
+
+function DeckPreview({
+  type,
+  onClose,
+  pageNumber,
+  file,
+  onDocumentLoadSuccess,
+  options,
+  numPages,
+  setPreviewPickDeckSlide,
+  setPageNumber,
+  deckId,
+}: Props) {
+  const { isShowModal, setShowModal, hasPasswordRequired } =
+    useContext(UIContext);
+
   const handleOnClose = (event) => {
     if (event.target.id === 'container') {
       onClose();
@@ -47,21 +55,58 @@ function DeckPreview({
     setPageNumber(pageNumber + 1);
   };
 
-  if (!visible) return null;
+  // const handleOnCloseFormClose = (event) => {
+  //   console.log(event.target);
+  //   if (event.target.id === 'containerClose') {
+  //     // setShowModal(false);
+  //   }
+  // };
 
-  const handleOnCloseFormClose = (event) => {
-    if (event.target.id === 'containerClose') {
+  const handleError = (error: Error | string) => {
+    let errorMessage: string = 'Whoops! Something went wrong. Error: ';
+    const contactSupportMessage = ' Please contact support.';
+    if (axios.isAxiosError(error)) {
+      errorMessage +=
+        error.response?.data?.message ??
+        error.response?.data ??
+        'Server error.';
+      enqueueSnackbar(errorMessage + contactSupportMessage, {
+        variant: 'error',
+        autoHideDuration: 10000,
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        },
+      });
+    } else {
+      errorMessage += (error as Error).message ?? error;
+      enqueueSnackbar(errorMessage + contactSupportMessage, {
+        variant: 'error',
+        autoHideDuration: 10000,
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        },
+      });
+    }
+  };
+
+  const handleModalSubmit = async (email: string, password?: string) => {
+    try {
+      if (hasPasswordRequired) {
+        await deckService.validateDeck({
+          _id: deckId as string,
+          password,
+        });
+      }
+      // TODO Here call deck-view service to create a new deck view, this will be updated later but is created now
+
       setShowModal(false);
+    } catch (error: any) {
+      handleError(error);
     }
   };
 
-  const handleModalSubmit = (email: string, password?: string) => {
-    console.log('Submitted Email:', email);
-    if (password !== '') {
-      console.log('Submitted Password:', password);
-    }
-    setShowModal(false);
-  };
   return (
     <div
       id="container"
@@ -72,87 +117,74 @@ function DeckPreview({
         type === 'deckUserPreview' ? 'p-4' : ''
       }`}
     >
-      {type === 'deckUserPreview' && (
-        <button
-          type="button"
-          className="hidden"
-          onClick={() => {
-            setShowModal(true);
-            document.body.style.overflow = 'hidden';
-          }}
-        >
-          Open Modal
-        </button>
-      )}
-      <AskEmailPassword
-        show={showModal}
-        onClose={handleOnCloseFormClose}
-        onSubmit={handleModalSubmit}
-        // showPasswordInput // You can change this to false if you only want an email input.
-      />
-      <Document
-        file={file}
-        onLoadSuccess={onDocumentLoadSuccess}
-        options={options}
-        noData={<h4 className="">No file selected</h4>}
-        className={`document h-screen p-4 bg-mirage ${
-          type === 'deckCreationPreview' ? ' rounded-lg' : 'md:rounded-none'
-        }`}
-      >
-        <div
-          className={`flex gap-4 justify-center ${
-            type === 'deckUserPreview' && 'w-full h-auto'
+      <AskEmailPassword onSubmit={handleModalSubmit} />
+
+      {!isShowModal && (
+        <Document
+          file={file}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+          noData={<h4 className="">No file selected</h4>}
+          className={`document h-screen p-4 bg-mirage ${
+            type === 'deckCreationPreview' ? ' rounded-lg' : 'md:rounded-none'
           }`}
         >
-          <Button
-            type="button"
-            icon={
-              <Logo
-                color={pageNumber === 1 ? '#f1511b2e' : '#F1511B'}
-                topLeft
-              />
-            }
-            onClick={onPrev}
-            disabled={pageNumber === 1}
-          />
-
-          <Page
-            renderTextLayer={false}
-            renderAnnotationLayer={false}
-            className={`${
-              type === 'deckUserPreview'
-                ? 'pageWrapperFullScreen'
-                : 'pageWrapper'
+          <div
+            className={`flex gap-4 justify-center${
+              type === 'deckUserPreview' ? ' w-full h-auto' : ''
             }`}
-            pageNumber={pageNumber}
-          />
-          <Button
-            type="button"
-            icon={
-              <Logo color={pageNumber === numPages ? '#f1511b2e' : '#F1511B'} />
-            }
-            className="max-h-min"
-            onClick={onNext}
-            disabled={pageNumber === numPages}
-          />
-        </div>
-        {type === 'deckCreationPreview' && (
-          <div className="previewPagesWrapper">
-            {Array.from(new Array(numPages), (el, index) => (
-              <Thumbnail
-                onItemClick={(args) => {
-                  setPreviewPickDeckSlide(true);
-                  console.log('args: ', args);
-                  setPageNumber(index + 1);
-                }}
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                className="previewPageWrapper"
-              />
-            ))}
+          >
+            <Button
+              type="button"
+              icon={
+                <Logo
+                  color={pageNumber === 1 ? '#f1511b2e' : '#F1511B'}
+                  topLeft
+                />
+              }
+              onClick={onPrev}
+              disabled={pageNumber === 1}
+            />
+
+            <Page
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+              className={`${
+                type === 'deckUserPreview'
+                  ? 'pageWrapperFullScreen'
+                  : 'pageWrapper'
+              }`}
+              pageNumber={pageNumber}
+            />
+            <Button
+              type="button"
+              icon={
+                <Logo
+                  color={pageNumber === numPages ? '#f1511b2e' : '#F1511B'}
+                />
+              }
+              className="max-h-min"
+              onClick={onNext}
+              disabled={pageNumber === numPages}
+            />
           </div>
-        )}
-      </Document>
+          {type === 'deckCreationPreview' && (
+            <div className="previewPagesWrapper">
+              {Array.from(new Array(numPages), (el, index) => (
+                <Thumbnail
+                  onItemClick={() => {
+                    setPreviewPickDeckSlide(true);
+                    setPageNumber(index + 1);
+                  }}
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  className="previewPageWrapper"
+                />
+              ))}
+            </div>
+          )}
+        </Document>
+      )}
 
       {type === 'deckUserPreview' && (
         <Button
@@ -166,19 +198,14 @@ function DeckPreview({
         />
       )}
 
-      {/* <button
-        className="buttonClose fixed w-8 h-8 top-8 right-8 p-2 bg-persimmon rounded-md"
-        onClick={onClose}
-        type="button"
-      >
-        <CloseIcon width="16" height="16" color="#FFFFFF" />
-      </button> */}
-      <Button
-        type="button"
-        icon={<CloseIcon width="16" height="16" color="#FFFFFF" />}
-        className="buttonClose fixed w-8 h-8 top-8 right-8 p-2 bg-persimmon rounded-md"
-        onClick={onClose}
-      />
+      {type === 'deckCreationPreview' && (
+        <Button
+          type="button"
+          icon={<CloseIcon width="16" height="16" color="#FFFFFF" />}
+          className="buttonClose fixed w-8 h-8 top-8 right-8 p-2 bg-persimmon rounded-md"
+          onClick={onClose}
+        />
+      )}
     </div>
   );
 }
