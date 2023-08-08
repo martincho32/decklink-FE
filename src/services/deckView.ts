e37997c72a31ac7b3212c50baf39b67195adb3e1
Martin Barros <martinbarros86@gmail.com>
2023-08-08 17:56:14 -0300

    Add 2 new services call

--- src/services/deckView.ts
+++ src/services/deckView.ts
@@ -11,6 +11,27 @@ export const getDeckViewByDeckId = (
   data: IDeckView[];
 }> => api.get(`${resource}/${id}`, config).then((data) => data);
 
+export const getDeckViewByDeckUrl = (
+  url: string,
+  config: AxiosRequestConfig<any> | undefined = undefined
+): Promise<{
+  data: IDeckView[];
+}> => api.get(`${resource}/url/${url}`, config).then((data) => data);
+
+export const getDeckViewByDeckCustomLink = (
+  url: string,
+  config: AxiosRequestConfig<any> | undefined = undefined
+): Promise<{
+  data: IDeckView[];
+}> => api.get(`${resource}/url/${url}`, config).then((data) => data);
+
+export const getDeckViewByNotStale = (
+  stale: boolean,
+  config: AxiosRequestConfig<any> | undefined = undefined
+): Promise<{
+  data: IDeckView[];
+}> => api.get(`${resource}/url/${stale}`, config).then((data) => data);
+
 export const createDeckView = (
   deckView: Partial<IDeckView>,
   config: AxiosRequestConfig<any> | undefined = undefined
