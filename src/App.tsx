b40c29e3a5c6ae04ce6e5f2ff00651aaacca7628
Chuprynsky Maxim <4oper321@gmail.com>
2023-08-11 22:17:35 +0300

    First testing of new package

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
 } from './pages';
 import ProtectedRoute from './components/ProtectedRoute';
 import { AuthProvider, UIProvider } from './context';
+import Test from './pages/FounderPart/Test';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
               <Route path="/landing" element={<Landing />} />
               <Route path="/login" element={<LogIn />} />
               <Route path="/signup" element={<SignUp />} />
+              <Route path="/test" element={<Test />} />
               <Route element={<ProtectedRoute />}>
                 <Route path="/founder/decks" element={<MyDecks />} />
                 <Route path="/founder/deck/create" element={<DeckCreation />} />
