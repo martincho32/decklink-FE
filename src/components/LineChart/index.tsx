41fb8136b971716b0c0312a378b6f2cb15417f2a
Chuprynsky Maxim <4oper321@gmail.com>
2023-08-14 21:22:50 +0300

    Fixing detailed view page styling

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -44,129 +44,13 @@ export default function LineChart({ deck, labels, data, pdfFile }: Props) {
       display: 'flex',
       justifyContent: 'center',
     }),
-    transformSize: ({ size }) => ({
-      height: size.height,
-      width: size.width + 5,
+    transformSize: () => ({
+      height: 176,
+      width: 300,
     }),
   };
 
-  // const getOrCreateTooltip = (chart, slideTitle: string) => {
-  //   let tooltipEl = chart.canvas.parentNode.querySelector('div');
-
-  //   if (!tooltipEl) {
-  //     tooltipEl = document.createElement('div');
-  //     tooltipEl.style.background = 'rgba(0, 0, 0, 0.7)';
-  //     tooltipEl.style.borderRadius = '3px';
-  //     tooltipEl.style.color = 'white';
-  //     tooltipEl.style.opacity = 1;
-  //     tooltipEl.style.pointerEvents = 'none';
-  //     tooltipEl.style.position = 'absolute';
-  //     tooltipEl.style.transform = 'translate(-50%, 0)';
-  //     tooltipEl.style.transition = 'all .1s ease';
-
-  //     const div = document.createElement('div');
-  //     // div.style.width = '100px';
-  //     // div.style.height = '100px';
-  //     const pageNumber = slideTitle.replace('Slide ', '');
-  //     const component = (
-  //       <DeckPreviewMini
-  //         fileUrl={deck?.deckUrl}
-  //         pageNumber={Number(pageNumber)}
-  //         onDocumentLoadSuccess={onDocumentLoadSuccess}
-  //       />
-  //     );
-  //     const htmlString = ReactDOMServer.renderToString(component);
-  //     console.log(htmlString);
-  //     div.innerHTML = htmlString;
-  //     tooltipEl.appendChild(div);
-  //     chart.canvas.parentNode.appendChild(tooltipEl);
-  //   }
-  //   return tooltipEl;
-  // };
-
-  // const externalTooltipHandler = (context) => {
-  //   // Tooltip Element
-  //   const { chart, tooltip } = context;
-  //   const tooltipEl = getOrCreateTooltip(chart, tooltip.title[0]);
-
-  //   // Hide if no tooltip
-  //   if (tooltip.opacity === 0) {
-  //     tooltipEl.style.opacity = 0;
-  //     return;
-  //   }
-
-  //   // Set Text
-  //   // if (tooltip.body) {
-  //   //   const titleLines = tooltip.title || [];
-  //   //   const bodyLines = tooltip.body.map((b) => b.lines);
-
-  //   //   const tableHead = document.createElement('thead');
-
-  //   //   titleLines.forEach((title) => {
-  //   //     const tr = document.createElement('tr');
-  //   //     tr.style.borderWidth = 0;
-
-  //   //     const th = document.createElement('th');
-  //   //     th.style.borderWidth = 0;
-  //   //     const text = document.createTextNode(title);
-
-  //   //     th.appendChild(text);
-  //   //     tr.appendChild(th);
-  //   //     tableHead.appendChild(tr);
-  //   //   });
-
-  //   //   const tableBody = document.createElement('tbody');
-  //   //   bodyLines.forEach((body, i) => {
-  //   //     const colors = tooltip.labelColors[i];
-
-  //   //     const span = document.createElement('span');
-  //   //     span.style.background = colors.backgroundColor;
-  //   //     span.style.borderColor = colors.borderColor;
-  //   //     span.style.borderWidth = '2px';
-  //   //     span.style.marginRight = '10px';
-  //   //     span.style.height = '10px';
-  //   //     span.style.width = '10px';
-  //   //     span.style.display = 'inline-block';
-
-  //   //     const tr = document.createElement('tr');
-  //   //     tr.style.backgroundColor = 'inherit';
-  //   //     tr.style.borderWidth = 0;
-
-  //   //     const td = document.createElement('td');
-  //   //     td.style.borderWidth = 0;
-
-  //   //     const text = document.createTextNode(body);
-
-  //   //     td.appendChild(span);
-  //   //     td.appendChild(text);
-  //   //     tr.appendChild(td);
-  //   //     tableBody.appendChild(tr);
-  //   //   });
-
-  //   //   const tableRoot = tooltipEl.querySelector('table');
-
-  //   //   // Remove old children
-  //   //   while (tableRoot.firstChild) {
-  //   //     tableRoot.firstChild.remove();
-  //   //   }
-
-  //   //   // Add new children
-  //   //   tableRoot.appendChild(tableHead);
-  //   //   tableRoot.appendChild(tableBody);
-  //   // }
-
-  //   const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
-
-  //   // Display, position, and set styles for font
-  //   tooltipEl.style.opacity = 1;
-  //   tooltipEl.style.left = positionX + tooltip.caretX + 'px';
-  //   tooltipEl.style.top = positionY + tooltip.caretY + 'px';
-  //   tooltipEl.style.font = tooltip.options.bodyFont.string;
-  //   tooltipEl.style.padding =
-  //     tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
-  // };
-
-  const widthNumberDesktop = deck && deck.slides && deck.slides * 300.5;
+  const widthNumberDesktop = deck && deck.slides && (deck.slides + 1) * 300;
 
   const widthStringDesktop = `${widthNumberDesktop}px`;
 
@@ -175,9 +59,7 @@ export default function LineChart({ deck, labels, data, pdfFile }: Props) {
     height: '12rem',
     display: 'flex',
   };
-  const previewDeckWidthNumber = deck && deck.slides && deck.slides * 300 + 100;
-
-  console.log(pdfFile);
+  const previewDeckWidthNumber = deck && deck.slides && deck.slides * 300;
 
   return (
     <div className="">
@@ -245,7 +127,7 @@ export default function LineChart({ deck, labels, data, pdfFile }: Props) {
           className="overflow-x-scroll h-auto ml-[-1rem] z-0"
         >
           <div
-            style={{ width: `calc(${widthNumberDesktop}px + 100px)` }} // Perform the subtraction with a numerical value
+            style={{ width: `${widthNumberDesktop}px` }} // Perform the subtraction with a numerical value
             className="h-[12rem] [&>*]:!h-[12rem]"
           >
             <Line
@@ -313,7 +195,7 @@ export default function LineChart({ deck, labels, data, pdfFile }: Props) {
               <Viewer
                 scrollMode={ScrollMode.Horizontal}
                 fileUrl={pdfFile} // Pass the PDF file URL to Viewer
-                enableSmoothScroll={false}
+                enableSmoothScroll
                 pageLayout={pageLayout}
                 defaultScale={SpecialZoomLevel.PageFit}
                 characterMap={{
