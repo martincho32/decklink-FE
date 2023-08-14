2e66ba975a16f7bb34d270d7062779c4b6edc776
Martin Barros <martinbarros86@gmail.com>
2023-08-14 18:39:04 -0300

    Clean comments from code

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -50,122 +50,6 @@ export default function LineChart({ deck, labels, data, pdfFile }: Props) {
     }),
   };
 
-  // const getOrCreateTooltip = (chart, slideTitle: string) => {
-  //   let tooltipEl = chart.canvas.parentNode.querySelector('div');
-
-  //   if (!tooltipEl) {
-  //     tooltipEl = document.createElement('div');
-  //     tooltipEl.style.background = 'rgba(0, 0, 0, 0.7)';
-  //     tooltipEl.style.borderRadius = '3px';
-  //     tooltipEl.style.color = 'white';
-  //     tooltipEl.style.opacity = 1;
-  //     tooltipEl.style.pointerEvents = 'none';
-  //     tooltipEl.style.position = 'absolute';
-  //     tooltipEl.style.transform = 'translate(-50%, 0)';
-  //     tooltipEl.style.transition = 'all .1s ease';
-
-  //     const div = document.createElement('div');
-  //     // div.style.width = '100px';
-  //     // div.style.height = '100px';
-  //     const pageNumber = slideTitle.replace('Slide ', '');
-  //     const component = (
-  //       <DeckPreviewMini
-  //         fileUrl={deck?.deckUrl}
-  //         pageNumber={Number(pageNumber)}
-  //         onDocumentLoadSuccess={onDocumentLoadSuccess}
-  //       />
-  //     );
-  //     const htmlString = ReactDOMServer.renderToString(component);
-  //     console.log(htmlString);
-  //     div.innerHTML = htmlString;
-  //     tooltipEl.appendChild(div);
-  //     chart.canvas.parentNode.appendChild(tooltipEl);
-  //   }
-  //   return tooltipEl;
-  // };
-
-  // const externalTooltipHandler = (context) => {
-  //   // Tooltip Element
-  //   const { chart, tooltip } = context;
-  //   const tooltipEl = getOrCreateTooltip(chart, tooltip.title[0]);
-
-  //   // Hide if no tooltip
-  //   if (tooltip.opacity === 0) {
-  //     tooltipEl.style.opacity = 0;
-  //     return;
-  //   }
-
-  //   // Set Text
-  //   // if (tooltip.body) {
-  //   //   const titleLines = tooltip.title || [];
-  //   //   const bodyLines = tooltip.body.map((b) => b.lines);
-
-  //   //   const tableHead = document.createElement('thead');
-
-  //   //   titleLines.forEach((title) => {
-  //   //     const tr = document.createElement('tr');
-  //   //     tr.style.borderWidth = 0;
-
-  //   //     const th = document.createElement('th');
-  //   //     th.style.borderWidth = 0;
-  //   //     const text = document.createTextNode(title);
-
-  //   //     th.appendChild(text);
-  //   //     tr.appendChild(th);
-  //   //     tableHead.appendChild(tr);
-  //   //   });
-
-  //   //   const tableBody = document.createElement('tbody');
-  //   //   bodyLines.forEach((body, i) => {
-  //   //     const colors = tooltip.labelColors[i];
-
-  //   //     const span = document.createElement('span');
-  //   //     span.style.background = colors.backgroundColor;
-  //   //     span.style.borderColor = colors.borderColor;
-  //   //     span.style.borderWidth = '2px';
-  //   //     span.style.marginRight = '10px';
-  //   //     span.style.height = '10px';
-  //   //     span.style.width = '10px';
-  //   //     span.style.display = 'inline-block';
-
-  //   //     const tr = document.createElement('tr');
-  //   //     tr.style.backgroundColor = 'inherit';
-  //   //     tr.style.borderWidth = 0;
-
-  //   //     const td = document.createElement('td');
-  //   //     td.style.borderWidth = 0;
-
-  //   //     const text = document.createTextNode(body);
-
-  //   //     td.appendChild(span);
-  //   //     td.appendChild(text);
-  //   //     tr.appendChild(td);
-  //   //     tableBody.appendChild(tr);
-  //   //   });
-
-  //   //   const tableRoot = tooltipEl.querySelector('table');
-
-  //   //   // Remove old children
-  //   //   while (tableRoot.firstChild) {
-  //   //     tableRoot.firstChild.remove();
-  //   //   }
-
-  //   //   // Add new children
-  //   //   tableRoot.appendChild(tableHead);
-  //   //   tableRoot.appendChild(tableBody);
-  //   // }
-
-  //   const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
-
-  //   // Display, position, and set styles for font
-  //   tooltipEl.style.opacity = 1;
-  //   tooltipEl.style.left = positionX + tooltip.caretX + 'px';
-  //   tooltipEl.style.top = positionY + tooltip.caretY + 'px';
-  //   tooltipEl.style.font = tooltip.options.bodyFont.string;
-  //   tooltipEl.style.padding =
-  //     tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
-  // };
-
   const widthNumberDesktop = deck && deck.slides && deck.slides * 300.5;
 
   const widthStringDesktop = `${widthNumberDesktop}px`;
@@ -176,8 +60,6 @@ export default function LineChart({ deck, labels, data, pdfFile }: Props) {
     display: 'flex',
   };
   const previewDeckWidthNumber = deck && deck.slides && deck.slides * 300 + 100;
-
-  console.log(pdfFile);
 
   return (
     <div className="">
