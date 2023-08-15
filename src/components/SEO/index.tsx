32a3a4972c50d7ad677f41d31d131f0742f79794
Martin Barros <martinbarros86@gmail.com>
2023-08-15 18:21:06 -0300

    Test adding more metatags

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -13,18 +13,30 @@ function SEO({ title, description, name, type }: Props) {
       {/* Standard metadata tags */}
       <title>{title}</title>
       <meta name="description" content={description} />
-      <link rel="canonical" href="https://www.tacobell.com/" />
       {/* End standard metadata tags */}
       {/* Facebook tags */}
       <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta
+        property="og:url"
+        content="https://decklink-fe-git-integration-martincho32.vercel.app/preview/oneeee"
+      />
+      <meta property="og:image" content="https://www.tacobell.com/" />
       {/* End Facebook tags */}
       {/* Twitter tags */}
       <meta name="twitter:creator" content={name} />
       <meta name="twitter:card" content={type} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      <meta
+        property="twitter:domain"
+        content="decklink-fe-git-integration-martincho32.vercel.app"
+      />
+      <meta
+        property="twitter:url"
+        content="https://decklink-fe-git-integration-martincho32.vercel.app/preview/oneeee"
+      />
       {/* End Twitter tags */}
     </Helmet>
   );
