f080b597c433bce2a6b3f0683443ce899a82dfb6
Chuprynsky Maxim <4oper321@gmail.com>
2023-08-16 10:07:49 +0300

    Watch cound added

--- src/components/FounderPart/DeckIndividualStats/index.tsx
+++ src/components/FounderPart/DeckIndividualStats/index.tsx
@@ -20,7 +20,58 @@ interface Props {
   deckViews: IDeckView[] | null;
 }
 
+interface IDeckViewNew extends IDeckView {
+  repeated: number;
+}
+
 function DeckIndividualStats({ deck, deckViews }: Props) {
+  console.log(deckViews);
+
+  // Step 1: Group objects by viewerEmail
+  const groupedData: Map<string | null | undefined, IDeckViewNew[]> = new Map();
+  const combinedData: IDeckViewNew[] = [];
+  const emailCountMap: Map<string, number> = new Map(); // To keep track of email counts
+
+  deckViews?.forEach((obj) => {
+    const { viewerEmail } = obj;
+    if (viewerEmail !== null && viewerEmail !== undefined) {
+      if (!groupedData.has(viewerEmail)) {
+        groupedData.set(viewerEmail, []);
+      }
+      groupedData.get(viewerEmail)?.push(obj);
+
+      // Increment email count
+      if (!emailCountMap.has(viewerEmail)) {
+        emailCountMap.set(viewerEmail, 1);
+      } else {
+        emailCountMap.set(viewerEmail, emailCountMap.get(viewerEmail)! + 1);
+      }
+    } else {
+      combinedData.push(obj);
+    }
+  });
+
+  // Step 2 and 3: Combine and sum viewingTime for each slide
+  groupedData.forEach((group, viewerEmail) => {
+    const combinedObject: IDeckViewNew = {
+      ...group[0], // You can copy other properties from the first object in the group
+      deckSlidesStats: group[0].deckSlidesStats.map((slide, index) => {
+        const combinedSlide = {
+          ...slide,
+          viewingTime: group.reduce(
+            (total, obj) => total + obj.deckSlidesStats[index].viewingTime,
+            0
+          ),
+        };
+        return combinedSlide;
+      }),
+      repeated: emailCountMap.get(viewerEmail as string) || 1, // Add the Repeated property
+    };
+    combinedData.push(combinedObject);
+  });
+
+  console.log(combinedData);
+
   const labels = Array.from(
     new Array(deck?.slides),
     (_el, index) => `Slide ${index + 1}`
@@ -30,7 +81,7 @@ function DeckIndividualStats({ deck, deckViews }: Props) {
     <>
       <div
         className={`flex flex-col items-center ${
-          !!deckViews?.length ? 'block' : 'hidden'
+          !!combinedData?.length ? 'block' : 'hidden'
         }`}
       >
         <h3 className="text-2xl leading-normal text-center">
@@ -39,8 +90,8 @@ function DeckIndividualStats({ deck, deckViews }: Props) {
         </h3>
         <span className="text-mirage">For each view</span>
       </div>
-      {!!deckViews?.length &&
-        deckViews.map((view) => {
+      {!!combinedData?.length &&
+        combinedData.map((view) => {
           return (
             <div key={view._id} className="mb-16">
               <Accordion
@@ -60,6 +111,7 @@ function DeckIndividualStats({ deck, deckViews }: Props) {
                       totalViewingTime={getTotalViewingTime(
                         view.deckSlidesStats
                       )}
+                      repeated={view.repeated}
                     />
                   </AccordionTrigger>
                   <AccordionContent>
