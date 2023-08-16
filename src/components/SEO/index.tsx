953063a79422e2824eb3a7b99f88682d9547a2d7
Martin Barros <martinbarros86@gmail.com>
2023-08-16 08:54:57 -0300

    Add title again in index.html Test with div in the SEO component

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -9,36 +9,39 @@ interface Props {
 
 function SEO({ title, description, name, type }: Props) {
   return (
-    <Helmet>
-      {/* Standard metadata tags */}
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      {/* End standard metadata tags */}
-      {/* Facebook tags */}
-      <meta property="og:type" content={type} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta
-        property="og:url"
-        content="https://decklink-fe-git-integration-martincho32.vercel.app/preview/oneeee"
-      />
-      <meta property="og:image" content="https://www.tacobell.com/" />
-      {/* End Facebook tags */}
-      {/* Twitter tags */}
-      <meta name="twitter:creator" content={name} />
-      <meta name="twitter:card" content={type} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta
-        property="twitter:domain"
-        content="decklink-fe-git-integration-martincho32.vercel.app"
-      />
-      <meta
-        property="twitter:url"
-        content="https://decklink-fe-git-integration-martincho32.vercel.app/preview/oneeee"
-      />
-      {/* End Twitter tags */}
-    </Helmet>
+    <div>
+      <Helmet>
+        {/* Standard metadata tags */}
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        {/* End standard metadata tags */}
+        {/* Facebook tags */}
+        <meta property="og:type" content={type} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:url"
+          content="https://decklink-fe-git-integration-martincho32.vercel.app/preview/oneeee"
+        />
+        <meta property="og:image" content="https://www.tacobell.com/" />
+        {/* End Facebook tags */}
+        {/* Twitter tags */}
+        <meta name="twitter:creator" content={name} />
+        <meta name="twitter:card" content={type} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta
+          property="twitter:domain"
+          content="decklink-fe-git-integration-martincho32.vercel.app"
+        />
+        <meta
+          property="twitter:url"
+          content="https://decklink-fe-git-integration-martincho32.vercel.app/preview/oneeee"
+        />
+        <meta name="twitter:image" content="https://www.tacobell.com/" />
+        {/* End Twitter tags */}
+      </Helmet>
+    </div>
   );
 }
 
